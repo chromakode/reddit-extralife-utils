7cf4d1f01e2c8108e8b1906ae0331be1adbcc353
Max Goodman <c@chromakode.com>
2012-10-19 23:59:15 -0700

    Couchify.

--- overlay/overlay.py
+++ overlay/overlay.py
@@ -1,82 +1,19 @@
-import sys
-import json
-import re
-from urllib2 import urlopen
-
-from bs4 import BeautifulSoup
 from flask import Flask, render_template
-from werkzeug.contrib.cache import SimpleCache
 
-
-PLAYERS = json.load(open(sys.argv[1]))
+from stats import Store
 
 
 app = Flask(__name__)
 app.jinja_env.line_statement_prefix = '%'
-cache = SimpleCache()
-
-
-def memoize(timeout=30):
-    def wrapper(f):
-        def wrapped(*args, **kwargs):
-            cache_key = 'memoize:' + f.__name__ + ':' + json.dumps([args, kwargs])
-            cached = cache.get(cache_key)
-            if cached:
-                return cached
-            val = f(*args, **kwargs)
-            cache.set(cache_key, val, timeout=timeout)
-            return val
-        return wrapped
-    return wrapper
-
-
-def track_changes(f):
-    def wrapped(*args, **kwargs):
-        cache_key = 'track_changes_last:' + f.__name__ + ':' + json.dumps([args, kwargs])
-        last = cache.get(cache_key)
-        val = f(*args, **kwargs)
-        cache.set(cache_key, val)
-        changed = last is not None and last != val
-        return val, changed
-    return wrapped
-
-
-@memoize()
-@track_changes
-def team_raised():
-    try:
-        team_url = 'http://www.extra-life.org/index.cfm?fuseaction=donorDrive.team&page=5&eventID=508&teamID=7951'
-        soup = BeautifulSoup(urlopen(team_url))
-        return soup.find('h5').stripped_strings.next()
-    except:
-        return '?'
-
-
-amount_re = re.compile(r'actualamount: "(\d+)"')
-@memoize()
-@track_changes
-def player_raised(xlid):
-    try:
-        player_url = 'http://www.extra-life.org/index.cfm?fuseaction=donorDrive.participant&participantID=%s' % xlid
-        soup = BeautifulSoup(urlopen(player_url))
-        script = soup.select('.thermoBox script')[0].text
-        amount = amount_re.search(script).group(1)
-        return '$' + amount
-    except:
-        return '?'
+store = Store()
 
 
 @app.route("/")
 def overlay():
-    total, total_changed = team_raised()
-
-    player_stats = []
-    for player in PLAYERS:
-        stats = player.copy()
-        stats['raised'], stats['raised_changed'] = player_raised(player['xlid'])
-        player_stats.append(stats)
-
-    return render_template('overlay.html', total=total, total_changed=total_changed, players=player_stats)
+    data = store.get_data()
+    total = data['total']
+    players = [ data['player:' + name] for name in data['players']['list'] ]
+    return render_template('overlay.html', total=total, players=players)
 
 
 if __name__ == "__main__":
---
+++ overlay/stats.py
@@ -0,0 +1,83 @@
+import sys
+import re
+import json
+from urllib2 import urlopen
+
+from bs4 import BeautifulSoup
+import couchdb
+
+
+class Store(object):
+    db_name = 'extralife'
+
+    def __init__(self):
+        self.server = couchdb.client.Server()
+
+        if not self.db_name in self.server:
+            self.server.create(self.db_name)
+        self.db = self.server[self.db_name]
+
+    def get_data(self):
+        return { row.id : row.doc for row in self.db.view('_all_docs', include_docs=True) }
+
+    def update(self, data):
+        for k, v in data.iteritems():
+            v['_id'] = k
+        self.db.update(data.values())
+
+
+def get_team_raised():
+    try:
+        team_url = 'http://www.extra-life.org/index.cfm?fuseaction=donorDrive.team&page=5&eventID=508&teamID=7951'
+        print >> sys.stderr, team_url
+        soup = BeautifulSoup(urlopen(team_url))
+        return soup.find('h5').stripped_strings.next()
+    except:
+        return '?'
+
+
+amount_re = re.compile(r'actualamount: "(\d+)"')
+def get_player_raised(xlid):
+    try:
+        player_url = 'http://www.extra-life.org/index.cfm?fuseaction=donorDrive.participant&participantID=%s' % xlid
+        print >> sys.stderr, player_url
+        soup = BeautifulSoup(urlopen(player_url))
+        script = soup.select('.thermoBox script')[0].text
+        amount = amount_re.search(script).group(1)
+        return '$' + amount
+    except:
+        return '?'
+
+
+def update(players_path):
+    players = json.load(open(players_path))
+    store = Store()
+
+    data = store.get_data()
+
+    total = get_team_raised()
+    data_total = data.get('total')
+    if not data_total:
+        data_total = data['total'] = {}
+    data_total['updated'] = total != data_total.get('value')
+    data_total['value'] = total
+
+    player_stats = []
+    for player in players:
+        player_key = 'player:' + player['name']
+        player_data = data.get(player_key)
+        if not player_data:
+            player_data = data[player_key] = player.copy()
+
+        raised = get_player_raised(player['xlid'])
+        player_data['raised_updated'] = raised != player_data.get('raised')
+        player_data['raised'] = raised
+
+    data['players'] = {'list': [ player['name'] for player in players ]}
+
+    store.update(data)
+    print >> sys.stderr, data
+
+
+if __name__ == "__main__":
+    update(sys.argv[1])
